c86dd2a2f7a86101345045afec339bc493989c06
sjxAndy <sjx18@mails.tsinghua.edu.cn>
2021-08-24 05:40:03 +0800

    for_act

--- cmds/run_act.sh
+++ cmds/run_act.sh
@@ -2,4 +2,4 @@ MODEL_FLAGS="--image_size 64 --num_channels 128 --num_res_blocks 3 --class_cond 
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
 TRAIN_FLAGS="--lr 1e-4 --batch_size 16"
 
-CUDA_VISIBLE_DEVICES=0 python scripts/image_train.py --data_dir  $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
+CUDA_VISIBLE_DEVICES=0 python scripts/image_train.py --data_dir /homes/55/yansong/action-to-motion/dataset $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
