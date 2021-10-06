5d7b1fbe6036c3f53ad40e058dc92ffc9259bb23
sjxAndy <88361848+sjxAndy@users.noreply.github.com>
2021-10-06 20:51:25 +0800

    Update run.sh

--- cmds/run.sh
+++ cmds/run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python ./scripts/image_train.py --data_dir /home/cgy/Dataset/nturgb+d_npy --image_size 32 --num_channels 128 --num_res_blocks 3 --diffusion_steps 4000 --noise_schedule linear --lr 1e-4 --batch_size 16
+CUDA_VISIBLE_DEVICES=1 python ../scripts/image_train.py --data_dir /home/cgy/Dataset/nturgb+d_npy --image_size 32 --num_channels 128 --num_res_blocks 3 --diffusion_steps 4000 --noise_schedule linear --lr 1e-4 --batch_size 16
